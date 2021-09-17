ad357b6446cd95148a80ea069798f4231ed995b7
4ndr32 <ademo18@gmail.com>
2021-09-17 14:53:15 -0500

    Proyecto terminado

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 numero = int(input("Digite el numero a verificar: "))
 
 cont = 0
-for i in range(numero):
+for i in range(1,numero + 1):
     if numero % i == 0:
         cont = cont + 1
+
 if cont == 2:
     print("el numero es primo")
 else:
