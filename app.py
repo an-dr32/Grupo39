456f07816605f2d8e93985949edfddf8039f8b1f
4ndr32 <ademo18@gmail.com>
2021-09-17 14:49:38 -0500

    no he podido con este error

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hola mundo")
+numero = input("Digite el numero a verificar: ")
+
+cont = 0
+for i in range(numero):
+    if numero % i == 0:
+        cont = cont + 1
+if cont == 2:
+    print("el numero es primo")
+else:
+    print("el numero no es primo")
+    
+print("El numero digitado es: ", numero)
